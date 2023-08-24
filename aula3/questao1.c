882ad9f52ab3f05696748453d6e06362f6afe4d6
LUIS EDUARDO MARINHO <107900365+DuduMarinho@users.noreply.github.com>
2023-08-24 14:19:18 -0300

    Add files via upload

---
+++ aula3/questao1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int media, num1, num2, num3;
+    num1 = 0;
+    num2 = 8;
+    num3 = 12;
+    media = (num1+num2+num3)/3;
+
+    printf("%d", media);
+
+    return 0;
+}
